27244dbd93e4638ccd5caec5abb39b07889305d0
emiliano <emiliontn@hotmail.com>
2023-08-22 16:50:36 -0600

    subir practica01

---
+++ initdb.sql
@@ -0,0 +1,10 @@
+CREATE USER myuser;
+CREATE DATABASE mydb;
+GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+create table ejemplo
+(
+        clave integer not null,
+        nombre character varying,
+        direccion character varying,
+        CONSTRAINT pk_clave primary key (clave)
+)
